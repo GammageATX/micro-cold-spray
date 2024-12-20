8c2163058aac8bf15b99fc396708e95235933ec7
Michael Gammage <60705964+GammageATX@users.noreply.github.com>
2024-12-20 14:33:45 -0600

    update 6 files and delete 30 files

--- src/micro_cold_spray/api/base/__init__.py
+++ src/micro_cold_spray/api/base/__init__.py
@@ -1,23 +1,5 @@
-"""Base API package."""
+"""Base error handling."""
 
 from micro_cold_spray.api.base.base_errors import create_error
-from micro_cold_spray.api.base.base_service import BaseService
-from micro_cold_spray.api.base.base_configurable import ConfigurableService
-from micro_cold_spray.api.base.base_registry import (
-    register_service,
-    get_service,
-    clear_services
-)
-from micro_cold_spray.api.base.base_router import BaseRouter
-from micro_cold_spray.api.base.base_app import create_app
 
-__all__ = [
-    'create_app',
-    'BaseService',
-    'ConfigurableService',
-    'BaseRouter',
-    'create_error',
-    'register_service',
-    'get_service',
-    'clear_services'
-]
+__all__ = ['create_error']
